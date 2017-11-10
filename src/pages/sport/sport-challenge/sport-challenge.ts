4004c693f864b17c0b9b615f3cfb0a13006fbf04
Mathilde Bronnert <mathildebronnert@MacBook-Air-de-Mathilde.local>
2017-11-10 11:13:33 +0100

    Add date to sport challenge

--- src/pages/sport/sport-challenge/sport-challenge.ts
+++ src/pages/sport/sport-challenge/sport-challenge.ts
@@ -6,9 +6,27 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'sport-challenge.html'
 })
 export class SportChallengePage {
+  date: any = new Date();
+  day: any;
+  month: any;
+
+  months: any = ["January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
 
   constructor(public navCtrl: NavController) {
-
+    this.day = this.date.getDate();
+    this.month = this.months[this.date.getMonth()];
   }
 
 }
