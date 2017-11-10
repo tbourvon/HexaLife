1427fd5e75916b8635d847e8e0c1c61fe742b572
Mathilde Bronnert <mathildebronnert@MacBook-Air-de-Mathilde.local>
2017-11-10 08:23:47 +0100

    Add food home page

--- src/pages/food/food-home/food.ts
+++ src/pages/food/food-home/food.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-food-home',
+  templateUrl: 'food-home.html'
 })
-export class ContactPage {
+export class FoodHomePage {
 
   constructor(public navCtrl: NavController) {
 
