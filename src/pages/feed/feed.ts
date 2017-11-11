be7dd461f52e2d19da137f98d9c9cc2379536cc1
Mathilde Bronnert <mathildebronnert@MacBook-Air-de-Mathilde.local>
2017-11-11 19:47:06 +0100

    Mur

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-feed',
@@ -7,8 +8,28 @@ import { NavController } from 'ionic-angular';
 })
 export class FeedPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
+  }
+
+  showConfirm(sn: string) {
+    let confirm = this.alertCtrl.create({
+      title: 'Connect to ' + sn,
+      message: 'The app would like to access to ' + sn + '. Do you agree?',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 
 }
