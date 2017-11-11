cf7f7b096abe5cb1a4e7f5bf97a200716407532f
hanhdroid <p.anh96@gmail.com>
2017-11-11 08:41:16 +0100

    add profile page

---
+++ src/pages/profile/profile-home/profile-home.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  templateUrl: 'profile-home.html'
+})
+export class ProfilePageHome {
+
+  slides = [
+    {
+      title: "Level: Max",
+      image: "../assets/imgs/championcup.png",
+      description: "You are a veteran. No one can stop you!",
+      text: "You are a machine! You've hit the 300km bar",
+    },
+    {
+    
+      title: "Sport",
+      description: "Statistics",
+    
+    },
+    {
+      title: "Nutrition",
+      description: "Statistics",
+    }
+  ];
+}
+
+
