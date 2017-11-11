20c78137e6dd7cf790e511cad4fac8f79d3b5e2b
hanhdroid <p.anh96@gmail.com>
2017-11-11 10:59:24 +0100

    add profile to tab

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { SportHomePage } from '../sport/sport-home/sport-home';
 import { FoodHomePage } from '../food/food-home/food-home';
 import { FoodStartersPage } from '../food/food-starters/food-starters';
+import { ProfilePageHome } from '../profile/profile-home/profile-home';
 
 @Component({
   selector: 'tabs',
@@ -13,7 +14,7 @@ export class TabsPage {
   tab1Root = SportHomePage;
   tab2Root = FoodHomePage;
   tab3Root = FoodStartersPage;
-
+  tab4Root = ProfilePageHome;
   constructor() {
 
   }
