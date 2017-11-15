8d1f12f3b55be625556fca669f00fb5f343df4e1
Mathilde Bronnert <mathildebronnert@MacBook-Air-de-Mathilde.local>
2017-11-15 18:42:42 +0100

    Change starters style

--- src/pages/food/food-starters/food-starters.ts
+++ src/pages/food/food-starters/food-starters.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FoodCoursesPage } from '../food-courses/food-courses';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-food-starters',
@@ -11,43 +12,47 @@ export class FoodStartersPage {
 	items = [
     {
         title: 'Mixed Salad',
+        number:0,
         emojis: ['em em-watermelon', 'em em-watermelon', 'em em-watermelon'],
-        calories : 50, 
-        fat: 10, 
+        calories : 50,
+        fat: 10,
         carbs: 20,
-        proteins: 40, 
+        proteins: 40,
         show: false
     },
     {
         title: 'Melon and Ham',
+        number:0,
         emojis: ['em em-watermelon', 'em em-watermelon'],
         calories : 70,
-        fat: 10, 
+        fat: 10,
         carbs: 20,
-        proteins: 40, 
+        proteins: 40,
         show: false
     },
     {
         title: 'Quiche',
+        number:0,
         emojis: ['em em-pizza'],
         calories : 100,
-        fat: 10, 
+        fat: 10,
         carbs: 20,
-        proteins: 40, 
+        proteins: 40,
         show: false
     },
     {
         title: 'Pizza',
+        number:0,
         emojis: ['em em-pizza', 'em em-pizza'],
         calories : 160,
-        fat: 10, 
+        fat: 10,
         carbs: 20,
         proteins: 40,
         show: false
     }
 	];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
 
@@ -59,5 +64,16 @@ export class FoodStartersPage {
   		event.stopPropagation();
         this.items[index].show = !this.items[index].show;
   }
+
+  minus(item) {
+    if(item.number > 0) {
+      item.number--;
+    }
+  }
+
+  plus(item) {
+    item.number++;
+  }
+
 
 }
